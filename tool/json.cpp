296d99ca5f3e0c4b82108ed8f2d72c3ab779349f
dingdinglz <1149978856@qq.com>
2024-12-07 16:07:16 +0800

    [dinglz] 添加json回应功能函数

---
+++ tool/json.cpp
@@ -0,0 +1,24 @@
+#include "json.h"
+#include "yyjson.h"
+
+std::string MakeStrAnswer(std::string text)
+{
+    auto doc = yyjson_mut_doc_new(NULL);
+    auto data = yyjson_mut_obj(doc);
+    yyjson_mut_doc_set_root(doc,data);
+    yyjson_mut_obj_add_str(doc,data,"data",text.c_str());
+    std::string ans = yyjson_mut_write(doc,0,NULL);
+    yyjson_mut_doc_free(doc);
+    return ans;
+}
+
+std::string MakeIntAnswer(int cnt)
+{
+    auto doc = yyjson_mut_doc_new(NULL);
+    auto data = yyjson_mut_obj(doc);
+    yyjson_mut_doc_set_root(doc,data);
+    yyjson_mut_obj_add_int(doc,data,"data",cnt);
+    std::string ans = yyjson_mut_write(doc,0,NULL);
+    yyjson_mut_doc_free(doc);
+    return ans;
+}
---
+++ tool/json.h
@@ -0,0 +1,7 @@
+#include <string>
+
+// MakeStrAnswer 用于生成一个函数的结果-Str
+std::string MakeStrAnswer(std::string text);
+
+// MakeIntAnswer 用于生成一个函数的结果-Int
+std::string MakeIntAnswer(int cnt);
