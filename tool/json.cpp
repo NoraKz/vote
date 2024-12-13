304411d0760195bc2498cfeb18d0540ace2671c4
dingdinglz <1149978856@qq.com>
2024-12-13 14:33:22 +0800

    [dinglz] 添加生成student list和getcall

--- tool/json.cpp
+++ tool/json.cpp
@@ -24,7 +24,29 @@ std::string MakeIntAnswer(int cnt)
     return ans;
 }
 
-std::string MakeStudentList(std::string className)
+std::string MakeStudentList(std::vector<Student> students)
 {
-    auto students = GetAllStudents(className);
+    if(students.size() == 0)
+    {
+        return R"JSON({"data":[]})JSON";
+    }
+    std::string start = R"JSON({"data":[)JSON";
+    for(auto & i : students)
+    {
+        std::string item = "{\"name\":\"" + i.name +"\",";
+        item += "\"id\":" + std::to_string(i.id) + ",";
+        item += "\"sex\":\"" + i.sex +"\"},";
+        start += item;
+    }
+    start = start.substr(0,start.size()-1);
+    start += "]}";
+    return start;
+}
+
+std::string GetCallString(std::string src , int index)
+{
+    yyjson_doc *doc = yyjson_read(src.c_str(), src.length() , 0);
+    yyjson_val *root = yyjson_doc_get_root(doc);
+    auto val = yyjson_arr_get(root , index);
+    return yyjson_get_str(val);
 }
--- tool/json.h
+++ tool/json.h
@@ -1,6 +1,8 @@
 #ifndef VOTE_JSON_HEADER
 #define VOTE_JSON_HEADER
 #include <string>
+#include <vector>
+#include "../student_database.h"
 
 // MakeStrAnswer 用于生成一个函数的结果-Str
 std::string MakeStrAnswer(std::string text);
@@ -8,5 +10,7 @@ std::string MakeStrAnswer(std::string text);
 // MakeIntAnswer 用于生成一个函数的结果-Int
 std::string MakeIntAnswer(int cnt);
 
-std::string MakeStudentList(std::string className);
+std::string MakeStudentList(std::vector<Student> students);
+
+std::string GetCallString(std::string src , int index);
 #endif
